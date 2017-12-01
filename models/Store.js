af34192b101ffae89346897693b8843e0dcaaf19
Ahmed Shaaban <ahmed.vuw@gmail.com>
2017-12-02 06:35:38 +1100

    added a form to save a store

--- models/Store.js
+++ models/Store.js
@@ -1,9 +1,27 @@
-// export.module = storeSchema {
-//   [
-//     {
-//       name: 'name',
-//       trim: true,
-//       required: true
-//     }
-//   ]
-// }
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const slug = require('slugs');
+
+const storeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: 'Please enter a store name!'
+  },
+  slug: String,
+  description: {
+    type: String,
+    trim: true
+  },
+  tags: [String]
+});
+storeSchema.pre('save', (next) => {
+  if(!this.isModified('name')){
+    next();
+    return;
+  }
+  this.slug = slug(this.name);
+  next();
+});
+
+module.exports = mongoose.model('Store', storeSchema);
